975edd893e02f1d2f4b968615d28f68918b1b2e7
Łukasz Łapiński <nofffy2005@gmail.com>
2026-04-26 15:14:20 +0200

    Removed code-behind

--- PresentationView/App.xaml.cs
+++ PresentationView/App.xaml.cs
@@ -1,38 +1,9 @@
 ﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-using PresentationView;
-using PresentationViewModel;
-using PresentationModel;
-using BusinessLogic;
 
 namespace PresentationView
 {
     public partial class App : Application
     {
-        private ServiceProvider _serviceProvider;
 
-        public App()
-        {
-            var services = new ServiceCollection();
-
-            services.AddSingleton<IBallLogic, BallLogic>();
-            services.AddSingleton<BallModel>();
-            services.AddSingleton<BallPresentationVM>();
-            services.AddTransient<MainWindow>();
-
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-            var viewModel = _serviceProvider.GetRequiredService<BallPresentationVM>();
-            
-            mainWindow.DataContext = viewModel;
-            
-            mainWindow.Show();
-        }
     }
 }
---
+++ PresentationView/VievModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic;
+using PresentationModel;
+using PresentationViewModel;
+
+namespace PresentationView
+{
+    public class ViewModelLocator
+    {
+        public BallPresentationVM MainViewModel { get; }
+
+        public ViewModelLocator()
+        {
+            IBallLogic ballLogic = new BallLogic();
+            BallModel ballModel = new BallModel(ballLogic);
+
+            MainViewModel = new BallPresentationVM(ballModel);
+        }
+    }
+}
